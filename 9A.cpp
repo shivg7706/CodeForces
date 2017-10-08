3ae98a526452c0d94fe68522d070a93c0593e9cc
shivg7706 <shivamgupta7706@gmail.com>
2017-10-08 15:58:36 +0530

    Add files via upload

---
+++ 9A.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,c;
+cin>>a>>b;
+c=max(a,b);
+if(c==1)
+cout<<"1/1";
+else if(c==2)
+cout<<"5/6";
+else if(c==3)
+cout<<"2/3";
+else if(c==4)
+cout<<"1/2";
+else if(c==5)
+cout<<"1/3";
+else
+cout<<"1/6";
+}
